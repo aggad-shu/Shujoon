520ca2afb8b8b7dc51c3451437c55417e868a569
aggad-shu <aggad.photos@gmail.com>
2026-07-10 21:28:57 +0000

    added scroll buttons

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
 
+document.querySelectorAll('.scroll-wrapper').forEach(wrapper => {
+
+    const row = wrapper.querySelector('.category-row');
+
+    wrapper.querySelector('.left').addEventListener('click', () => {
+        row.scrollBy({
+            left: -400,
+            behavior: 'smooth'
+        });
+    });
+
+    wrapper.querySelector('.right').addEventListener('click', () => {
+        row.scrollBy({
+            left: 400,
+            behavior: 'smooth'
+        });
+    });
+
+});
