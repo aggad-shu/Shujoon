8e5f75f79ae76959891842ddd4fd7610a0158ea6
aggad-shu <aggad.photos@gmail.com>
2026-09-01 16:29:24 +0000

    working on nav bar

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const navLinks = document.querySelector(".nav-links");
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
 });
 
 document.querySelectorAll('.scroll-wrapper').forEach(wrapper => {
@@ -23,4 +24,21 @@ document.querySelectorAll('.scroll-wrapper').forEach(wrapper => {
         });
     });
 
+});
+
+let lastScrollY = window.scrollY;
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY && currentScrollY > 80) {
+        // Scrolling down
+        navbar.classList.add("navbar-hidden");
+    } else {
+        // Scrolling up
+        navbar.classList.remove("navbar-hidden");
+    }
+
+    lastScrollY = currentScrollY;
 });
